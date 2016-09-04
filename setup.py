6c5355b3fe51a19a3970f94328d6e6a8a4170a7d
Neal Fultz <nfultz@gmail.com>
2016-09-03 17:24:22 -0700

    minimal setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nb2mail',
+      version='0.1',
+      description='Convert notebooks to email',
+      url='http://github.com/nfultz/nb2mail',
+      author='Neal Fultz',
+      author_email='nfultz@gmail.com',
+      license='BSD',
+      packages=['nb2mail'],
+      zip_safe=False)
